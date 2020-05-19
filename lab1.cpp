85b1c304acff93bf0370f729402ca25cbf035efd
lapitan <60895080+lapitan@users.noreply.github.com>
2020-05-19 18:51:28 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include "PGM.h";
+#include "PPM.h";
+
+using namespace std;
+
+struct pixel
+{
+
+	unsigned char red;
+	unsigned char green;
+	unsigned char blue;
+
+};
+
+int main(int argc, char* argv[]) {
+
+	if (argc != 4) {
+		cout << "command is invalid\n";
+		exit(0);
+	}
+	string finName = string(argv[1]);
+	string foutName = string(argv[2]);
+	string command = string(argv[3]);
+	ifstream fin;
+	/*string finName = "xmm.ppm";
+	string foutName = "xmmout.ppm";
+	string command = "103";*/
+	try
+	{
+		fin.open(finName,ios::binary);
+	}
+	catch (const std::exception& ex)
+	{
+		cout << ex.what();
+	}
+
+	char vers[2];
+	try
+	{
+		fin >> vers[0] >> vers[1];
+	}
+	catch (const std::exception& ex)
+	{
+		cout << ex.what();
+	}
+	if (!(vers[0] == 'P' && (vers[1] == '5' || vers[1] == '6'))) {
+		cout << "Version is invalid\n";
+		exit(0);
+	}
+
+	PNM* picture;
+	//picture = new PPM(finName);
+	try
+	{
+		if (vers[1] == '5') {
+			picture = new PGM(finName);
+		}
+		else {
+			picture = new PPM(finName);
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		cout << ex.what();
+		exit(0);
+	}
+
+	try
+	{
+		for (int i = 0; i < command.length(); i++) {
+			if (command[i] == '0') {
+				picture->ColorInversion();
+			}
+			else if (command[i] == '1') {
+				picture->HorizontalReflect();
+			}
+			else if (command[i] == '2') {
+				picture->VerticalReflect();
+			}
+			else if (command[i] == '3') {
+				picture->RotateRight();
+			}
+			else if (command[i] == '4') {
+				picture->RotateLeft();
+			}
+			else {
+				throw exception("Invalid command");
+			}
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		cout << ex.what();
+	}
+
+	try
+	{
+		picture->write(foutName);
+	}
+	catch (const std::exception& ex)
+	{
+		ex.what();
+	}
+	return 0;
+
+}
